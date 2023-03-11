3c5f3a36fefdfa0ac7779cc46c4301c9c5853231
mac-wangsh <46606405@qq.com>
2023-03-11 21:38:27 +0800

    chore: playground

---
+++ playground/vite.config.ts
@@ -0,0 +1,16 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@yy-web/template': path.resolve(__dirname, '../src/index.ts'),
+    },
+  },
+})
